450929a35c273c60053049873e171d2486c3ff34
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2024-05-29 01:25:50 +0300

    Update LogoutBtn.jsx

--- src/components/LogoutBtn/LogoutBtn.jsx
+++ src/components/LogoutBtn/LogoutBtn.jsx
@@ -13,7 +13,7 @@ const LogoutBtn = () => {
     <Button type="button" onClick={handleLogout}>
       <Icon>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-          <use href={`https://kir2023.github.io/e-pharmacy-frontend/${sprite}#icon-logout`} fill="var(--main-color)" className="icon-logout" />
+          <use href={`${sprite}#icon-logout`} fill="var(--main-color)" className="icon-logout" />
         </svg>
       </Icon>
     </Button>
