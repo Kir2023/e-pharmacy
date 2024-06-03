00a4f13e66ae6a9dd448c0bdfe5ce14cbad57535
Kir2023 <kirill.kaydanovskyi@gmail.com>
2024-06-03 21:33:23 +0300

    add login/logout logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+// import { Suspense, lazy } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import SharedLayout from './components/SharedLayout/SharedLayout';
+// import Loader from './components/Loader/Loader';
+
+// const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+// const DasboardPage = lazy(() => import('./pages/DasboardPage/DasboardPage'));
+// const AllOrdersPage = lazy(() => import('./pages/AllOrdersPage/AllOrdersPage'));
+// const AllProductsPage = lazy(() => import('./pages/AllProductsPage/AllProductsPage'));
+// const AllSuppliersPage = lazy(() => import('./pages/AllSuppliersPage/AllSuppliersPage'));
+// const CustomersDataPage = lazy(() => import('./pages/CustomersDataPage/CustomersDataPage'));
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Suspense fallback={<Loader/>}>
+//         <Routes>
+//           <Route path="/login" element={<LoginPage />} />
+//           <Route path="/" element={<SharedLayout />}>
+//             <Route path="dashboard" element={<DasboardPage />} />
+//             <Route path="orders" element={<AllOrdersPage />} />
+//             <Route path="products" element={<AllProductsPage />} />
+//             <Route path="suppliers" element={<AllSuppliersPage />} />
+//             <Route path="customers" element={<CustomersDataPage />} />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import Loader from './components/Loader/Loader';
+import PrivateRoute from './routes/PrivateRoute';
 
 const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
-const DasboardPage = lazy(() => import('./pages/DasboardPage/DasboardPage'));
+const DashboardPage = lazy(() => import('./pages/DashboardPage/DashboardPage'));
 const AllOrdersPage = lazy(() => import('./pages/AllOrdersPage/AllOrdersPage'));
 const AllProductsPage = lazy(() => import('./pages/AllProductsPage/AllProductsPage'));
 const AllSuppliersPage = lazy(() => import('./pages/AllSuppliersPage/AllSuppliersPage'));
@@ -13,11 +49,11 @@ const CustomersDataPage = lazy(() => import('./pages/CustomersDataPage/Customers
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<SharedLayout />}>
-            <Route path="dashboard" element={<DasboardPage />} />
+          <Route path="/" element={<PrivateRoute><SharedLayout /></PrivateRoute>}>
+            <Route path="dashboard" element={<DashboardPage />} />
             <Route path="orders" element={<AllOrdersPage />} />
             <Route path="products" element={<AllProductsPage />} />
             <Route path="suppliers" element={<AllSuppliersPage />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Logo from "../../components/Logo/Logo";
 import LogoutBtn from "../../components/LogoutBtn/LogoutBtn";
 import SubTitle from "../../components/SubTitle/SubTitle";
@@ -5,12 +6,23 @@ import PageTitle from "../../components/Title/Title";
 import { Wrapper } from "./Header.styled";
 
 const Header = () => {
+
+  const handleLogout = async () => {
+    try {
+      await axios.get('https://e-pharmacy-backend-ez9m.onrender.com/api/user/logout');
+      localStorage.removeItem('token');
+      console.log('Logout successful');
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
   return (
     <Wrapper>
       <Logo />
       <PageTitle title="Medicine Store" />
       <SubTitle text="Dashboard" />
-      <LogoutBtn />
+      <LogoutBtn onClick={handleLogout} />
     </Wrapper>
   );
 };
