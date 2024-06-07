4040618d0fef000820f417cd0772caf487ee0d13
Kir2023 <Kir2023@users.noreply.github.com>
2024-06-07 14:45:55 +0000

    deploy: b8d3d397aa81dcfb1046840d1da8b5bab944ea1b

--- assets/AllProductsPage-7IxOXAWG.js
+++
@@ -1,1 +0,0 @@
-import{j as r,C as t}from"./index-D1lS88Pl.js";const e=()=>r.jsx(t,{children:"AllProductsPage"});export{e as default};
---
+++ assets/AllSuppliersPage-CRnr6lca.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CvTBbUrB.js";const l=()=>e.jsx("div",{children:"AllSuppliersPage"});export{l as default};
